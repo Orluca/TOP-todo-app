48c8d2e3bb4d51665d14ee152b654025827368a2
Orluca <67704819+Orluca@users.noreply.github.com>
2022-08-15 11:06:50 +0200

    Added basic footer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import index from "./index.css";
-import Sidebar from "./modules/sidebar/sidebar.js";
+import indexStyle from "./index.css";
 import Header from "./modules/header/header.js";
+import Footer from "./modules/footer/footer.js";
 
 function app() {
   const content = document.querySelector(".content");
 
   content.appendChild(Header());
+  content.appendChild(Footer());
 }
 
 app();
