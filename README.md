ae032c3543fc2a285a3bbd2720561d32d03f364a
Orluca <67704819+Orluca@users.noreply.github.com>
2022-08-15 12:40:05 +0200

    Added basic sidebar button

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TEMPLATE-webpack
 
 - Build based on https://www.youtube.com/watch?v=IZGNcSuwBZs
+
+### icon credit:
+
+- FLATICON: freepik
--- src/modules/main/sidebar/sidebar.js
+++ src/modules/main/sidebar/sidebar.js
@@ -1,8 +1,55 @@
 import sidebarStyle from "./sidebar.css";
+import iconToday from "./assets/icon-today.png";
 
+// --------------------- TODAYS TASKS BUTTON ---------------------
+function todayIcon() {
+  const todayIcon = document.createElement("img");
+  todayIcon.classList.add("sidebar-btn-icon");
+  todayIcon.src = iconToday;
+
+  return todayIcon;
+}
+
+function todayText() {
+  const todayText = document.createElement("div");
+  todayText.textContent = "TODAY";
+
+  return todayText;
+}
+
+function todayLeft() {
+  const todayLeft = document.createElement("div");
+  todayLeft.classList.add("sidebar-btn-left");
+
+  todayLeft.appendChild(todayIcon());
+  todayLeft.appendChild(todayText());
+
+  return todayLeft;
+}
+
+function todayCount() {
+  const todayCount = document.createElement("div");
+  todayCount.textContent = "6";
+
+  return todayCount;
+}
+
+function today() {
+  const today = document.createElement("div");
+  today.classList.add("sidebar-btn");
+
+  today.appendChild(todayLeft());
+  today.appendChild(todayCount());
+
+  return today;
+}
+
+// --------------------- SIDEBAR ---------------------
 function sidebar() {
   const sidebar = document.createElement("div");
   sidebar.classList.add("sidebar");
+
+  sidebar.appendChild(today());
 
   return sidebar;
 }
