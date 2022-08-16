26059ec916a10510da1ccc4c7ce98d4c9c6c3df3
Orluca <67704819+Orluca@users.noreply.github.com>
2022-08-16 20:38:36 +0200

    Added inputs

--- src/modules/UserInterface/Main/TodoListContainer/AddTodoModal/AddTodoModal.js
+++ src/modules/UserInterface/Main/TodoListContainer/AddTodoModal/AddTodoModal.js
@@ -29,7 +29,57 @@ const AddTodoModalComponents = (function () {
     const modalWindow = document.createElement("div");
     modalWindow.classList.add("add-todo-modal-window");
 
+    modalWindow.appendChild(inputTitle());
+    modalWindow.appendChild(inputDescription());
+    modalWindow.appendChild(inputDate());
+    modalWindow.appendChild(inputPriority());
+
     return modalWindow;
+  }
+
+  function inputTitle() {
+    const inputTitle = document.createElement("div");
+
+    inputTitle.innerHTML = `
+      <label for="title-input">Title:</label>
+      <input type="text" id="title-input"/>
+    `;
+
+    return inputTitle;
+  }
+
+  function inputDescription() {
+    const inputDescription = document.createElement("div");
+
+    inputDescription.innerHTML = `
+      <label for="description-input">Description:</label>
+      <textarea id="description-input"></textarea>
+    `;
+
+    return inputDescription;
+  }
+
+  function inputDate() {
+    const inputDate = document.createElement("div");
+
+    inputDate.innerHTML = `
+      <label for="date-input">Date:</label>
+      <input type="datetime-local" id="date-input"/>
+    `;
+
+    return inputDate;
+  }
+
+  function inputPriority() {
+    const inputPriority = document.createElement("select");
+
+    inputPriority.innerHTML = `
+      <option value="low">🟢 Low</option>
+      <option value="medium">🟡 Medium</option>
+      <option value="high">🔴 High</option>
+    `;
+
+    return inputPriority;
   }
 
   return { modalWindow };
