a148032dea67099d9630c33b9d777e3998d2b492
Orluca <67704819+Orluca@users.noreply.github.com>
2022-08-23 11:00:33 +0200

    Sidebar basic structure

---
+++ src/modules/Interface/Main/Sidebar/Sidebar.js
@@ -0,0 +1,21 @@
+import sidebarStyle from "./Sidebar.css";
+import iconToday from "../../../assets/icon-today.svg";
+import iconWeek from "../../../assets/icon-week.svg";
+import iconPlus from "../../../assets/icon-plus.svg";
+// import TodoList from "../TodoListContainer/TodoList/TodoList.js";
+// import Data from "../../../Data/Data.js";
+// import { ModalWindow } from "../TodoListContainer/AddTodoModal/AddTodoModal";
+
+const Sidebar = (function () {})();
+
+const TodayButton = (function () {})();
+
+const WeekButton = (function () {})();
+
+const ProjectsHeader = (function () {})();
+
+const ProjectsList = (function () {})();
+
+const ProjectsEdit = (function () {})();
+
+export default Sidebar;
