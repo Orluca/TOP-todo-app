7b0ed9ae8f5a27c18671e471fc4f43e607c7b202
Orluca <67704819+Orluca@users.noreply.github.com>
2022-08-23 10:32:00 +0200

    Basic main component

---
+++ src/modules/Interface/Main/Main.js
@@ -0,0 +1,24 @@
+import mainStyle from "./Main.css";
+import Sidebar from "./Sidebar/Sidebar.js";
+import Content from "./Content/Content.js";
+
+const Main = (function () {
+  let Main;
+
+  function create() {
+    Main = document.createElement("div");
+    Main.classList.add("main");
+    Main.appendChild(Sidebar.get());
+    Main.appendChild(Content.get());
+  }
+
+  function get() {
+    return Main;
+  }
+
+  create();
+
+  return { get };
+})();
+
+export default Main;
