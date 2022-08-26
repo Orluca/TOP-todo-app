839ec7ec25c643f3692886b439bd0d38046a6bae
Orluca <67704819+Orluca@users.noreply.github.com>
2022-08-26 11:39:47 +0200

    Added icon to sidebar toggle

--- src/modules/Interface/Header/Header.js
+++ src/modules/Interface/Header/Header.js
@@ -1,13 +1,23 @@
 import HeaderStyle from "./Header.css";
 import Sidebar from "../Main/Sidebar/Sidebar.js";
+import iconMenu from "../../assets/icon-sidebar-toggle.svg";
 
 const Header = (function () {
   let Header;
 
   function SidebarToggle() {
+    function Icon() {
+      const icon = document.createElement("img");
+      icon.classList.add("sidebar-toggle-icon");
+      icon.src = iconMenu;
+
+      return icon;
+    }
+
     const SidebarToggle = document.createElement("button");
-    SidebarToggle.textContent = "Sidebar";
     SidebarToggle.addEventListener("click", Sidebar.toggle);
+    SidebarToggle.classList.add("sidebar-toggle");
+    SidebarToggle.appendChild(Icon());
 
     return SidebarToggle;
   }
